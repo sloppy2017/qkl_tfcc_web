f83e813ab33c3b51be745640df4055a4b4483202
zhi jian <1406900462@qq.com>
2016-09-12 15:55:19 +0800

    1111111111

--- WebContent/resources/qkl_tfcc/js/register/tabslist.js
+++ WebContent/resources/qkl_tfcc/js/register/tabslist.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
 		$(' .form >form').eq(index).show().siblings().hide();
 	})
 })
-$('.mark1-box .real-name').bind('mousedown',function(){
-	$('.mark1-box .real-name1').show();
-})
-$('.mark1-box .later-on').bind('mousedown',function(){
-	$('.mark1-box .later-on1').show();
-})
+
+
+//$('.mark1-box .real-name').bind('mousedown',function(){
+//	$('.mark1-box .real-name1').show();
+//})
+//$('.mark1-box .later-on').bind('mousedown',function(){
+//	$('.mark1-box .later-on1').show();
+//})
