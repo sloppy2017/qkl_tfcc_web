8dda5be519add6aa1d4ca5b791273f1021f172e1
Administrator <Administrator@PC201608311226>
2016-09-22 17:25:47 +0800

    ss

--- WebContent/resources/qkl_tfcc/js/general_vip/mystock.js
+++ WebContent/resources/qkl_tfcc/js/general_vip/mystock.js
@@ -14,7 +14,7 @@ function getBankAccInfo(){
         dataType: "json",
         success: function (data) {	
         	 if(data.success){
-             		 $("#revorgname").text(data.data.payeeName);
+             		 $("#revorgname").text(data.data.orgName);
              		 $("#revbankaccno").text(data.data.bankaccno);
              		/* $("#revbankdepname").text(data.data.depositBankname);
              		 $("#checkphone").text(data.data.checkphone);*/
@@ -77,11 +77,10 @@ $('#buy').click(function(){//提交购买申请
             
             
             if(data.message=='订单已生成，请及时付款'){
-            	 $(function(){
-//                 window.location.href="https://auth.alipay.com/login/index.htm";
-                 window.open("https://auth.alipay.com/login/index.htm",'width:300','height:300');
-                 	
-                 });
+            
+                 window.location.href="https://auth.alipay.com/login/index.htm";
+//                 window.open("https://auth.alipay.com/login/index.htm",'width:300','height:300');
+                
             }
            
             	
